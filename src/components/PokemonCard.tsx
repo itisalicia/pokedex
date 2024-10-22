ed62d8f2df009f96e7d223002e9fe3e83f3e529b
ALICIA <alicia.magro@gmail.com>
2024-10-22 11:45:15 +0200

    third quest is done

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,9 +1,29 @@
 function PokemonCard() {
-    return <><figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-            alt="Bulbasaur" />
-    </figure> 
-    <figcaption> Bulbasaur</figcaption></>
+  const pokemon = pokemonList[0];
+  return (
+    <figure>
+      {pokemon.imgSrc ? (
+        <p>
+          {" "}
+          <img src={pokemon.imgSrc} alt="Bulbasaur" />
+        </p>
+      ) : (
+        <p> ??</p>
+      )}
+      <figcaption> {pokemon.name}</figcaption>
+    </figure>
+  );
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 export default PokemonCard;
